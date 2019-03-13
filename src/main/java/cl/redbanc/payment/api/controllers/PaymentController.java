ec057aa8ed881c8541b6adc8547fcc21be1b1ce5
apibaque <47745995+apibaque@users.noreply.github.com>
2019-03-13 15:17:09 -0300

    Update PaymentController.java

--- src/main/java/cl/redbanc/payment/api/controllers/PaymentController.java
+++ src/main/java/cl/redbanc/payment/api/controllers/PaymentController.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 import cl.redbanc.payment.api.model.PaymentDTO;
 import cl.redbanc.payment.service.PaymentService;
 
@@ -22,6 +27,9 @@ public class PaymentController {
 	@Autowired
 	PaymentService paymentService;
 	
+	private final static Logger logger = LoggerFactory.getLogger(PaymentController.class);
+	
+	
 	/**
 	 *
 	 * @param name
@@ -30,19 +38,25 @@ public class PaymentController {
 	 */
 	@RequestMapping(method = RequestMethod.POST, value = "/payments/consent")
 	public ResponseEntity<PaymentDTO> addUser(@RequestBody PaymentDTO payment) {
+		logger.info("Request body (ADD consent) from API: " + payment.toString());
 		PaymentDTO response = paymentService.addPayment(payment);
+		logger.info("Response body (ADD consent) from API: " + response.toString());
 		return new ResponseEntity<>(response, HttpStatus.CREATED);
 	}
 	
 	@RequestMapping(method=RequestMethod.POST, value="/payments")
 	public ResponseEntity<PaymentDTO> update(@RequestBody PaymentDTO payment) {
+		logger.info("Request body (ADD payment) from API: " + payment.toString());
 		PaymentDTO response = paymentService.updatePayment(payment);
+		logger.info("Response body (ADD payment) from API: " + response.toString());
 		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/payments/{id}")
 	public ResponseEntity<PaymentDTO> findById(@PathVariable String id) {
+		logger.info("Request body (GET payment) from API idOrder: " + id);
 		PaymentDTO response = paymentService.getPayment(id);
+		logger.info("Response body (GET payment) from API idOrder: " + id + " data: " + response.toString());
 		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
 }
